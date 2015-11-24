edf1f5651e79153ad1d614a792ee840a6e090f69
chengang <chengang@cpan.org>
2015-11-24 20:27:09 +0800

    some fix

--- server/server.js
+++ server/server.js
@@ -3,22 +3,45 @@ var WebSocketServer = require('ws').Server
 
 console.log('Server started on 12626');
 
-var
-rabbit = {x:0, y:0};
+var playerPos = {x:0, y:0};
+
+function wsBroadCast(cmd, data) {
+    for(var i in wss.clients) {
+        wss.clients[i].send(JSON.stringify({"cmd": cmd, "data":data}));
+    }
+}
+
+function controllerSyncPos(data) {
+    playerPos.x = data.x;
+    playerPos.y = data.y;
+    wsBroadCast("syncPos", playerPos);
+}
+
+function controllerGetGlobal(data) {
+    var global = {"random_seed": 5};
+    wsBroadCast("getGlobal", global);
+}
 
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-        var incommingMsg = JSON.parse(message);
-        rabbit.x = incommingMsg.x;
-        rabbit.y = incommingMsg.y;
-        for(var i in wss.clients) {
-            wss.clients[i].send(JSON.stringify(rabbit));
+        var msg = JSON.parse(message);
+        var cmd = msg["cmd"];
+        var data = msg["data"];
+        var resp;
+        console.log('msg: ' + cmd);
+        switch(cmd) {
+            case "syncPos":
+                resp = controllerSyncPos(data);
+                break;
+            case "getGlobal":
+                resp = controllerGetGlobal(data);
+                break;
         }
-        console.log('message');
+        console.log('msg: ' + cmd);
     });
     wss.clients.forEach(function each(client) {
-        client.send(JSON.stringify(rabbit));
+        client.send(JSON.stringify(playerPos));
     });
-    //ws.send(JSON.stringify(rabbit));
+    //ws.send(JSON.stringify(playerPos));
     console.log('connection');
 });
