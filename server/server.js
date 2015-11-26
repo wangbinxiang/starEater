2adc9303898f78aac2d67cafe81d93cd08ad793b
wangbinxiang <45696104@qq.com>
2015-11-26 17:07:53 +0800

    进入index.html页面，服务器自动创建一个玩家进入等待队列，有两名玩家等待队列时，服务器创建房间，并发送消息给玩家前台页面

--- server/server.js
+++ server/server.js
@@ -1,11 +1,92 @@
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({port: 12626});
 
+//uuid
+var uuid = require('node-uuid');
+
 console.log('Server started on 12626');
 
 var playerPos = {x:0, y:0};
 var rooms = {};
-var waitingPlayers = [];
+
+//玩家类
+function player(ws){
+    this.uuid = uuid.v4();
+    this.ws = ws;
+}
+
+//创建玩家
+function createPlayer(ws) {
+    return new player(ws);
+}
+
+//玩家等待队列对象
+function waitingPlayers() {
+    this.players = [];
+}
+
+//循环方法扫描等待玩家
+waitingPlayers.prototype.scanWaiterToCreateRoom = function() {
+    console.log('scaning.....' + this.players.length);
+    //判断当前等待用户是否多余一个
+    if (this.players.length > 1) {
+
+        //获取玩家数据，创建房间，通知玩家加入房间
+        //获取一号玩家
+        playerOne = this.players.shift();
+        //获取二号玩家
+        playerTwo = this.players.shift();
+
+        //创建房间
+        room = createRoom();
+        //房间加入房间列表
+        rooms[room.urid] = room;
+        //一号玩家加入房间
+        room.playerOne = playerOne;
+        //二号玩家加入房间
+        room.playerTwo = playerTwo;
+
+        //通知玩家加入房间
+        this.notifyPlayerJoinRoom(room);
+        //继续等待玩家
+        this.scanWaiterToCreateRoom();
+    } else {
+        //延时扫描等待玩家
+        var _this = this;
+        setTimeout(function(){_this.scanWaiterToCreateRoom()}, 5000);
+    }
+}
+
+//通知玩家加入房间
+waitingPlayers.prototype.notifyPlayerJoinRoom = function(room) {
+    //通知一号玩家加入房间
+    room.playerOne.ws.send('something');
+    //通知二号玩家加入房间
+    room.playerTwo.ws.send('something');
+}
+
+//玩家加入等待队列
+waitingPlayers.prototype.playerJoinWaiting = function(player) {
+    //通知一号玩家加入房间
+    this.players.push(player)
+}
+
+
+//房间类
+function room() {
+    //房间id
+    this.urid = uuid.v4();
+    //一号玩家，左边
+    this.playerOne = null;
+    //二号玩家，右边
+    this.playerTwo = null;
+}
+
+
+//创建房间
+function createRoom() {
+    return new room();
+}
 
 function wsBroadCast(cmd, data) {
     for(var i in wss.clients) {
@@ -25,6 +106,12 @@ function controllerInit(data) {
     wsBroadCast("init", global);
 }
 
+
+//创建队列
+var wp = new waitingPlayers();
+//开始扫描队列
+wp.scanWaiterToCreateRoom()
+
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         var msg = JSON.parse(message);
@@ -33,6 +120,11 @@ wss.on('connection', function(ws) {
         var resp;
         switch(cmd) {
             case "init":
+                //初始化时，创建玩家
+                var player = createPlayer(ws);
+                //玩家加入等待队列
+                wp.playerJoinWaiting(player);
+
                 resp = controllerInit(data);
                 break;
             case "syncPos":
